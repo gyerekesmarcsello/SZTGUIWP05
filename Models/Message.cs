d5098877d9d69ec822a90bb4bee8e61c861a6e3a
gyerekesmarcsello <hallgato@LABOR210-12>
2022-03-22 12:16:32 +0100

    Message osztály

---
+++ Models/Message.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SZTGUIWorkhop5.Models
+{
+    class Message
+    {
+        public Message(string name, string directmessage)
+        {
+            this.Name = name;
+            this.DirectMessage = directmessage;
+            this.localDate = DateTime.Now;
+        }
+
+        public string Name { get; set; }
+
+        public string DirectMessage { get; set; }
+
+        public DateTime localDate { get; set; }
+    }
+}
